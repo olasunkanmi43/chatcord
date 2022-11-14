4e4d84d11ee1c2e1fbf98635e97f6d8bc32c465e
mac <mac@macs-MacBook-Air.local>
2022-11-14 12:16:23 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'ChatCord Bot';
+
+// RUN WHEN CLIENT CONNECTS
+io.on('connection', socket => { 
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+socket.join(user.room);
+
+// WELCOME CURRENT USER
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+// BROADCAST WHEN A USER CONNECTS
+    socket.broadcast.to(user.room).emit(
+        'message', 
+        formatMessage(botName, `${user.username} has joined the chat`)
+        );
+
+// SEND USERS AND ROOM INFO
+    io.to(user.room).emit('roomUsers', { 
+    room: user.room, 
+    users: getRoomUsers(user.room)
+}); 
+
+ });
+
+
+ // LISTEN FOR CHATMESSAGE
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+
+       io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+
+ // RUNS WHEN CLIENT DISCONNECTS
+     socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+        if(user) {
+        io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`)
+        );
+
+        // SEND USERS AND ROOM INFO
+    io.to(user.room).emit('roomUsers', { 
+        room: user.room, 
+        users: getRoomUsers(user.room)
+    }); 
+    
+        }
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
